fa0617bfb702535a54eae0f90551de1a831778f9
Nathan Nderitu <natenderitu@gmail.com>
2024-04-09 19:23:17 +0300

    chore: initial setup

---
+++ src/theme/Theme.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+  palette :{
+    primary: {
+      main: '#ff00dd'
+    },
+    secondary:{
+      main: '#ee1155'
+    }
+  }
+});
+
+type ThemeProps = {
+    children: React.ReactNode;
+
+}
+
+export default function Theme({children}: ThemeProps){
+    return(
+        <ThemeProvider theme = {theme}>
+            {children}
+        </ThemeProvider>
+    );
+}
