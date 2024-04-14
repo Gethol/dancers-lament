936ee0d5a4f2aab2e9d869b2202c158c9bb27b03
Nathan Nderitu <natenderitu@gmail.com>
2024-04-14 23:17:02 +0300

    fix: align text in the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,13 +6,20 @@ export default function Hero(){
     return (
         <Container sx={{
             height: '100vh',
-            background:'#ccc',
+            // background:'#ccc',
             display: 'flex',
+            flexDirection:  'column',
+            justifyContent: 'center'
         }}>
-            <Typography variant= 'h1'>Dancers Lament</Typography>
-            <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro expedita cum beatae assumenda corporis aliquid architecto est! Eligendi, vero! Sunt repudiandae voluptates saepe ea ut doloribus ullam nemo mollitia velit.</Typography>
+            <Typography variant= 'h1' sx ={{
+                textAlign: 'center'
+            }}>Dancers Lament</Typography>
+            <Typography variant="body1"  sx ={{
+                textAlign: 'center'
+            }}>Lorem ipsum dolor sit amet consectetur
+             adipisicing elit. Porro expedita cum beatae assumenda corporis aliquid architecto est! Eligendi, vero! Sunt repudiandae voluptates saepe ea ut doloribus ullam nemo mollitia velit.</Typography>
             <Box>
-                <Button variant></Button>
+                {/* <Button variant =""></Button> */}
             </Box>
         </Container>
     );
